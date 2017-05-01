12c40e1e521ed6b11b6b84d980395864883ba64e
Chris Andrejewski <christopher.andrejewski@gmail.com>
2017-05-01 16:06:22 -0600

    initial core

---
+++ core/resolvers/gist.js
@@ -0,0 +1,7 @@
+module.exports = {
+    isMatch (url) {},
+
+    getInfoForUrl (url) {},
+
+    getExecutorForUrl (url) {}
+}
---
+++ core/work-dir.js
@@ -0,0 +1,44 @@
+let i = 0
+
+function makeDir (path) {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(path, error => (
+            error ? reject(error) : resolve()
+        ))
+    })
+}
+
+module.exports = {
+    createWorkDir (parentDir) {
+        return new Promise((resolve, reject) => {
+            let attmepts = 0
+            let error
+            let filepath
+            while (attmepts++ < 3) {
+                try {
+                    name = 'work-dir-' + (i++)
+                    filepath = path.join(directory, name)
+                    await makeDir(filepath)
+                } catch (caughtError) {
+                    error = caughtError
+                    continue
+                }
+                error = null
+                break
+            }
+            if (error) {
+                reject(error)
+            } else {
+                resolve(filepath)
+            }
+        })
+    },
+
+    removeWorkDir (dir) {
+        return new Promise((resolve, reject) => {
+            rimraf(dir, function (error) {
+                error ? reject(error) : resolve()
+            })
+        })
+    }
+}
